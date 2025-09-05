06700f2abd2d348c18bebfe58d51a432e75abf96
Rudra Verma <rudraverma781@gmail.com>
2025-09-05 16:30:27 +0530

    Made some changes

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import assets from "../assets/assets"
+import { useNavigate } from "react-router-dom"   // ✅ fix import
 
 export default function Navbar() {
+  const navigate = useNavigate()
+
   return (
     <div className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32 cursor-pointer '>
-        <img src={assets.logo} alt="logo" className='w-32 sm:w-44' />
-        <button>
-            Login
-            <img src={assets.arrow} alt="arrow" className='w-3' />
-        </button>
-
+      <img 
+        onClick={() => navigate('/')} 
+        src={assets.logo} 
+        alt="logo" 
+        className='w-32 sm:w-44' 
+      />
+      
+      <button  
+        onClick={() => navigate('/admin')}   // ✅ fixed bracket
+        className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5'
+      >
+        Login
+        <img src={assets.arrow} alt="arrow" className='w-3' />
+      </button>
     </div>
   )
 }
