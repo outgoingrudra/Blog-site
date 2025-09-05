5fe87318bea521390838af2f6c9262bae99e7c78
Rudra Verma <rudraverma781@gmail.com>
2025-09-05 10:38:47 +0530

    2 pages added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Home from "./Pages/Home"
+import Blog from "./Pages/Blog"
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+       <Routes >
+          <Route path ='/' element={<Home/>} />
+          <Route path ='/blog' element={<Blog/>} />
+
+       </Routes>
+
+
+    </div>
   )
 }
