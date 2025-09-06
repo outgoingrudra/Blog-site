3a5a217cff679598b4677bc2e1522f037c4c7675
Rudra Verma <rudraverma781@gmail.com>
2025-09-06 09:02:58 +0530

    changes made

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import assets from '../assets/assets'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='mx-8 sm:mx-16 xl:mx-24 relative'>
+        <div className="">
+            <div className="text-center mt-20 mb-8"
+            ><p>AI feature Integrated</p>
+                <img src={assets.star_icon} className='w-2.5' alt="" />
+            </div>
+
+        </div>
+        <img src={assets.gradientBackground} alt="" className='absolute -top-50 -z-1 opacity-50' />
+
+
+    </div>
   )
 }
+
