4f6a9941aacf3e2ec41c8b3dd9285c7bb6ed0e01
Rudra Verma <rudraverma781@gmail.com>
2025-09-06 20:13:40 +0530

    changes made

--- frontend/src/Components/BlogList.jsx
+++ frontend/src/Components/BlogList.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { blogCategories } from '../assets/assets'
 
 export default function BlogList() {
+    const [menu,setMenu] =useState("All")
   return (
     <div>
         <div className=" flex justify-center gap-4 sm:gap-8 my-10 relative ">
             {/* criteria listing here  */}
             {blogCategories.map((item)=>(
                 <div key={item} className='relative'>
-                    <button className='cursor-pointer text-gray-500 '>{item}
+                    <button onClick={()=>setMenu(item)}
+                     className={`cursor-pointer text-gray-500 ${menu===item && 'text-white px-4 pt-0.5'}`}>{item}
                         <div className="absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full"></div>
                     </button>
                 </div>
