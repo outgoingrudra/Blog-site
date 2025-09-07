a9edcc180d093bc93440faa9eb76ee7cae694222
Rudra Verma <rudraverma781@gmail.com>
2025-09-07 13:44:56 +0530

    changes made

--- frontend/src/Components/BlogCard.jsx
+++ frontend/src/Components/BlogCard.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
 export default function BlogCard({blog}) {
+    const {title,description , category,image,_id}=blog
   return (
-    <div>BlogCard</div>
+    <div>
+        <img src={image} alt="" className='aspect-video' />
+        <span className=' ml-5 mt-4 px-3 py-1 inline-block bg-primary/20 rounded-full text-primary text-xs'
+        >{category}</span>
+        <div className="">
+            <h5>{title}</h5>
+            <p>{description.slice(0,80)}</p>
+        </div>
+    </div>
   )
 }
