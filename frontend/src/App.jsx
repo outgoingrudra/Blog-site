9a28ba89c5e7d0314c2fc4e74ba92555560c020b
Rudra Verma <rudraverma781@gmail.com>
2025-09-07 14:02:48 +0530

    changes made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import {Routes,Route} from 'react-router-dom'
-import Home from "./Pages/Home"
-import Blog from "./Pages/Blog"
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Blog from "./Pages/Blog";
 
 export default function App() {
   return (
     <div>
-       <Routes >
-          <Route path ='/' element={<Home/>} />
-          <Route path ='/blog' element={<Blog/>} />
-
-       </Routes>
-
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog/:id" element={<Blog />} />
+      </Routes>
     </div>
-  )
+  );
 }
