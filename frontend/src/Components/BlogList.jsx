8dd81fa387a9660fc335b628c82d6ab8726e228c
Rudra Verma <rudraverma781@gmail.com>
2025-09-07 13:38:56 +0530

    changes made

--- frontend/src/Components/BlogList.jsx
+++ frontend/src/Components/BlogList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { blogCategories } from '../assets/assets'
+import {motion} from "motion/react"
 
 export default function BlogList() {
     const [menu,setMenu] =useState("All")
@@ -11,7 +12,10 @@ export default function BlogList() {
                 <div key={item} className='relative'>
                     <button onClick={()=>setMenu(item)}
                      className={`cursor-pointer text-gray-500 ${menu===item && 'text-white px-4 pt-0.5'}`}>{item}
-                       {menu===item && ( <div className="absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full"></div>)}
+                       {menu===item && ( <motion.div 
+                       layoutId='underline'
+                       transition={{type:'spring',stiffness:500,damping:30}}
+                       className="absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full"></motion.div>)}
                     </button>
                 </div>
             ))}
