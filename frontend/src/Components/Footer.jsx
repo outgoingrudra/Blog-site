e333ab3c8d315050cc076051d5362fdbd1ec4280
Rudra Verma <rudraverma781@gmail.com>
2025-09-08 16:43:12 +0530

    changes made

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div >
+       <div className="">
+
+       </div>
+       <p className='py-4 text-center text-sm md:text-base text-gray-500'>Copyright  2025 &copy-<a href="https://github.com/outgoingrudra" className='text-black font-bold'>Rudra Verma</a> QuickBlog All Right Reserved . </p>
+
+
+    </div>
   )
 }
