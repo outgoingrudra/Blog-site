42ea1ce2b5aa6b26cbd5377dc7d6a90527af5aab
Rudra Verma <rudraverma781@gmail.com>
2025-09-08 16:35:51 +0530

    changes made

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from '../Components/Navbar'
 import Header from '../Components/Header'
 import BlogList from '../Components/BlogList'
+import Newsletter from '../Components/Newsletter'
+import Footer from '../Components/Footer'
 
 export default function Home() {
   return (
@@ -10,6 +12,8 @@ export default function Home() {
    <Navbar/>
    <Header/>
    <BlogList/>
+   <Newsletter/>
+   <Footer/>
     </div>
   )
 }
