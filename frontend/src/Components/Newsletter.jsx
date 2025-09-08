774deb61d2dc76dd27ad3eb4e6c1eb1d332c8bca
Rudra Verma <rudraverma781@gmail.com>
2025-09-08 16:34:14 +0530

    changes made

--- frontend/src/Components/Newsletter.jsx
+++ frontend/src/Components/Newsletter.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React from "react";
 
 export default function Newsletter() {
   return (
-    <div>
-      <h1>Never Miss a blog </h1>
-      <p className="">
-
-      </p>
-      <form action="" className='flex items-center justify-between max-w-2xl w-full md:h-13 h-12'>
-        <input className='border border-gray-300 rounded-md h-full border-r-0 outline-none w-full rounded-r-none px-3 text-gray-500' type="text" placeholder='Enter Your email id ' required/>
-        <button type='submit' 
-        className='md:px-12 px-8 h-full text-white bg-primary/80 hover:bg-primary transition-all cursor-pointer rounded-md rounded-l-none'
-        >SUBSCRIBE</button>
+    <div className="flex flex-col items-center justify-center text-center space-y-2  my-32">
+      <h1 className="md:text-4xl text-2xl font-semibold">Never Miss a blog </h1>
+      <p className="md:text-lg text-gray-500/70 pb-8 "></p>
+      <form
+        action=""
+        className="flex items-center justify-between max-w-2xl w-full md:h-13 h-12"
+      >
+        <input
+          className="border border-gray-300 rounded-md h-full border-r-0 outline-none w-full rounded-r-none px-3 text-gray-500"
+          type="text"
+          placeholder="Enter Your email id "
+          required
+        />
+        <button
+          type="submit"
+          className="md:px-12 px-8 h-full text-white bg-primary/80 hover:bg-primary transition-all cursor-pointer rounded-md rounded-l-none"
+        >
+          SUBSCRIBE
+        </button>
       </form>
-
     </div>
-  )
+  );
 }
