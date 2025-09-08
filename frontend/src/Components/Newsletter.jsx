53092fd481e6b70c99d5adfb907f5107fb7a8d6a
Rudra Verma <rudraverma781@gmail.com>
2025-09-08 16:32:06 +0530

    changes made

--- frontend/src/Components/Newsletter.jsx
+++ frontend/src/Components/Newsletter.jsx
@@ -7,8 +7,8 @@ export default function Newsletter() {
       <p className="">
 
       </p>
-      <form action="">
-        <input className='border border-gray-300' type="text" placeholder='Enter Your email id ' required/>
+      <form action="" className='flex items-center justify-between max-w-2xl w-full md:h-13 h-12'>
+        <input className='border border-gray-300 rounded-md h-full border-r-0 outline-none w-full rounded-r-none px-3 text-gray-500' type="text" placeholder='Enter Your email id ' required/>
         <button type='submit' 
         className='md:px-12 px-8 h-full text-white bg-primary/80 hover:bg-primary transition-all cursor-pointer rounded-md rounded-l-none'
         >SUBSCRIBE</button>
