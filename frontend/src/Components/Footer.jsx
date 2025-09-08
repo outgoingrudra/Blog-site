b415b94bf78a8724f284d569dc53d280da53d576
Rudra Verma <rudraverma781@gmail.com>
2025-09-08 16:50:25 +0530

    changes made

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React from "react";
+import  assets  from "../assets/assets";
 
 export default function Footer() {
   return (
-    <div >
-       <div className="">
-
-       </div>
-       <p className='py-4 text-center text-sm md:text-base text-gray-500'>Copyright  2025 &copy-<a href="https://github.com/outgoingrudra" className='text-black font-bold'>Rudra Verma</a> QuickBlog All Right Reserved . </p>
-
-
+    <div className="px-6 md:px-16 lg:px-24 xl:px-32 bg-primary/3">
+      <div className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500">
+        <div className="">
+          <img src={assets.logo} alt="" className="w-32 sm:w-44" />
+          <p className="max-w-[410px] mt-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste veniam
+            perspiciatis aut explicabo nam neque nihil ratione fugiat saepe
+            maxime?
+          </p>
+        </div>
+      </div>
+      <p className="py-4 text-center text-sm md:text-base text-gray-500">
+        Copyright 2025 &copy-
+        <a
+          href="https://github.com/outgoingrudra"
+          className="text-black font-bold"
+        >
+          Rudra Verma
+        </a>{" "}
+        QuickBlog All Right Reserved .{" "}
+      </p>
     </div>
-  )
+  );
 }
