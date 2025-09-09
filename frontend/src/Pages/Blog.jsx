c1587ef1dcdd4c9711d57a6de7036ff509cc4ef7
Rudra Verma <rudraverma781@gmail.com>
2025-09-09 14:20:50 +0530

    changes made

--- frontend/src/Pages/Blog.jsx
+++ frontend/src/Pages/Blog.jsx
@@ -18,8 +18,11 @@ export default function Blog() {
     <div className='relative'>
       <img src={assets.gradientBackground} alt="" className='absolute -top-50 -z-1 opacity-50' />
       <Navbar/>
-           <div className="">
-            <p>Published on {Moment(data.createdAt).format('MMMM Do YYYY')}</p>
+           <div className="text-center mt-20 text-gray-600">
+            <p className='text-primary py-4 font-medium'>Published on {Moment(data.createdAt).format('MMMM Do YYYY')}</p>
+            <h1 className='text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto  text-gray-800'>{data.title}</h1>
+            <h2 className='my-5 max-w-lg truncate mx-auto'>{data.subTitle}</h2>
+            <p className='inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/5 font-medium text-primary'>Michael Brown</p>
             
             </div>
             <div className="">  
