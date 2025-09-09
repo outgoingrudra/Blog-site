985bcacb1c5b073a351725e505f6832a5a9e4d8e
Rudra Verma <rudraverma781@gmail.com>
2025-09-09 13:54:05 +0530

    changes made

--- frontend/src/Pages/Blog.jsx
+++ frontend/src/Pages/Blog.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {useParams} from 'react-router-dom'
+import {blog_data} from "../assets/assets" 
 
 export default function Blog() {
+  const {id} =useParams()
+  const [data ,setdata] = useState(null)
+  const fetchBlogdata=async ()=>{
+   const data =  blog_data.find(item=>item._id==id)
+   setdata(data)
+   
+
+  }
   return (
     <div>Blog</div>
   )
