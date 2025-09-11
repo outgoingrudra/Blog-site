cb69bd2c534e827fe5971eff45fe362d191f6cf3
Rudra Verma <rudraverma781@gmail.com>
2025-09-11 21:43:33 +0530

    changes made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Blog from "./Pages/Blog";
+import Layout from "./Pages/Admin/Layout";
 
 export default function App() {
   return (
@@ -9,6 +10,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/admin" element={<Layout />} />
+
       </Routes>
     </div>
   );
