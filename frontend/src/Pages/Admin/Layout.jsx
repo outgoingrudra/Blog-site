2839001d638604b6a3708d8eedc1db7b35db633a
Rudra Verma <rudraverma781@gmail.com>
2025-09-12 08:51:46 +0530

    changes made

--- frontend/src/Pages/Admin/Layout.jsx
+++ frontend/src/Pages/Admin/Layout.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import assets from '../../assets/assets'
+import {useNavigate} from "react-router-dom"
 
 export default function Layout() {
+  const navigate = useNavigate()
+
+  const logout =()=>{
+    navigate("/")
+  }
   return (
    <>
-       <div className="">
-           <img src={assets.logo} alt="" className='w-32 sm:w-40 cursor-pointer' />
+       <div className="flex  items-center justify-between  py-2  h-[70px] px-4 sm:px-12  border-b border-gray-200">
+           <img src={assets.logo} alt="" className='w-32 sm:w-40 cursor-pointer' onClick={()=>navigate('/')} />
+           <button onClick={logout} className='text-sm px-8 py-2  bg-primary text-white rounded-full  cursor-pointer'>Log Out</button>
        </div>
    </>
   )
