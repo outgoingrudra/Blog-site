000a1d0246dfc88d33a0450f453007219c81f262
Rudra Verma <rudraverma781@gmail.com>
2025-09-12 09:05:53 +0530

    changes made

--- frontend/src/Pages/Admin/Layout.jsx
+++ frontend/src/Pages/Admin/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import assets from '../../assets/assets'
-import {useNavigate} from "react-router-dom"
+import {Outlet, useNavigate} from "react-router-dom"
+import Sidebar from '../../Components/admin/Sidebar'
 
 export default function Layout() {
   const navigate = useNavigate()
@@ -13,6 +14,10 @@ export default function Layout() {
        <div className="flex  items-center justify-between  py-2  h-[70px] px-4 sm:px-12  border-b border-gray-200">
            <img src={assets.logo} alt="" className='w-32 sm:w-40 cursor-pointer' onClick={()=>navigate('/')} />
            <button onClick={logout} className='text-sm px-8 py-2  bg-primary text-white rounded-full  cursor-pointer'>Log Out</button>
+       </div>
+       <div className="flex h-[calc(100vh-70px)]">
+             <Sidebar/>
+             <Outlet/>
        </div>
    </>
   )
