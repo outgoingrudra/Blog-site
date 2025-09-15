758e3829602ccb8ba045809093a78fee460106e5
Rudra Verma <rudraverma781@gmail.com>
2025-09-15 07:43:47 +0530

    changes made

--- frontend/src/Components/admin/Login.jsx
+++ frontend/src/Components/admin/Login.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className='flex items-center justify-center h-screen'>
+          <div className="">
+            <div className="
+            "></div>
+          </div>
+
+    </div>
   )
 }
