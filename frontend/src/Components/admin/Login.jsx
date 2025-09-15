e41890ea0132b4695d68f68cd821fa992e455d6f
Rudra Verma <rudraverma781@gmail.com>
2025-09-15 19:30:31 +0530

    changes made

--- frontend/src/Components/admin/Login.jsx
+++ frontend/src/Components/admin/Login.jsx
@@ -3,9 +3,18 @@ import React from 'react'
 export default function Login() {
   return (
     <div className='flex items-center justify-center h-screen'>
-          <div className="">
-            <div className="
-            "></div>
+          <div className="w-full max-w-sm p-6  max-md:m-6 border border-primary/30
+          shadow-xl shadow-primary/15 rounded-lg">
+            <div className=" w-full py-6 text-center
+            ">
+                <h1 className="text-3xl font-bold">
+                  <span className="text-primary">Admin</span> Login
+                  <p className='font-light'>
+                    Enter your credentials to access admin panel
+                  </p>
+                </h1>
+
+            </div>
           </div>
 
     </div>
