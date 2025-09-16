0e368441b1eefc2c32bca09a05a3f67c56e259b6
Rudra Verma <rudraverma781@gmail.com>
2025-09-16 07:20:51 +0530

    changes made

--- frontend/src/Pages/Admin/Dashboard.jsx
+++ frontend/src/Pages/Admin/Dashboard.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import assets, { dashboard_data } from '../../assets/assets'
 
 export default function Dashboard() {
+  const [dashData,setdashData]v= useState({
+    blogs:0,
+    comments:0 ,
+    drafts : 0,
+    recentBlogs:[]
+
+  })
+
+  const fetchData =async ()=>{
+    setdashData(dashboard_data)
+
+  }
+  useEffect(()=>{
+    fetchData()
+  },[])
   return (
-    <div>Dashboard</div>
+    <div className='flex -1 p-4 md:p-10  bg-blue-50/50'>
+             
+             <div className="flex flex-wrap gap-4">
+              <div className="">
+                  <img src={assets.dashboard_icon_1} alt="" />
+                  <div className="">
+                    <p>{das}</p>
+                  </div>
+              </div>
+
+             </div>
+
+    </div>
   )
 }
