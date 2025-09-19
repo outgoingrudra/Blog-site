b38dada28fb06ac5a0553e4dbd95bb776a17b0d7
Rudra Verma <rudraverma781@gmail.com>
2025-09-19 18:16:02 +0530

    changes made

--- frontend/src/Pages/Admin/ListBlog.jsx
+++ frontend/src/Pages/Admin/ListBlog.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import {blog_data} from "../../assets/assets"
 export default function ListBlog() {
+  const [blogs,setBlogs] = useState([])
+
+  const fetchBlogs=async()=>{
+    setBlogs(blog_data)
+
+  }
+
+  useEffect(()=>{
+    fetchBlogs()
+  },[])
   return (
     <div>ListBlog</div>
   )
