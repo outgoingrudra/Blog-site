542d47880415f4f535aa715479d2cb7ce4fa8cdb
Rudra Verma <rudraverma781@gmail.com>
2025-09-19 20:53:42 +0530

    changes made

--- frontend/src/Pages/Admin/Comments.jsx
+++ frontend/src/Pages/Admin/Comments.jsx
@@ -1,8 +1,30 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { comments_data } from '../../assets/assets'
 
 export default function Comments() {
   const [comments,setComments] = useState([])
+  const [filter , setFilter] = useState('Not Approved')
+
+  const fetchComment = async()=>{
+    setComments(comments_data)
+  }
+  useEffect(()=>{
+    fetchComment()
+  },[])
   return (
-    <div>Comments</div>
+    <div className='flex pt-5 px-5  sm:pt-12  sm:pl-16 bg-blue-50/50'>
+      <div className="flex justify-between items-center max-w-3xl">
+        <h1 className="">
+          Comments
+        </h1>
+        <div className="flex gap-4">
+          <button className={`shadow-custom-sm border rounded-full px-4 py-1  cursor-pointer text-xs ${filter == 'Approved' ? 'text-primary' : 'text-gray-700'}`}>Approved</button>
+
+        </div>
+      </div>
+
+
+
+    </div>
   )
 }
