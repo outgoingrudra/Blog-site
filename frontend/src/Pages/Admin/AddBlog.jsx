8b9983d122eaeeaf3a354021c691f60ce51a9511
Rudra Verma <rudraverma781@gmail.com>
2025-09-22 20:38:01 +0530

    changes made

--- frontend/src/Pages/Admin/AddBlog.jsx
+++ frontend/src/Pages/Admin/AddBlog.jsx
@@ -1,49 +1,77 @@
-import React, { useState } from 'react'
-import assets from '../../assets/assets'
+import React, { useState } from "react";
+import assets from "../../assets/assets";
 
 export default function AddBlog() {
-   const [image ,setImage] = useState(false)
-   const [title ,setTitle] = useState('')
-   const [subtitle ,setSubtitle] = useState('')
-   const [category ,setcategory] = useState('startup')
-   const [isPublished ,setisPublished] = useState(false)
+  const [image, setImage] = useState(false);
+  const [title, setTitle] = useState("");
+  const [subtitle, setSubtitle] = useState("");
+  const [category, setcategory] = useState("startup");
+  const [isPublished, setisPublished] = useState(false);
 
-
-   const  onSubmitHandler =(e)=>{
+  const onSubmitHandler = (e) => {
     e.prevantDefault();
+  };
 
-   }
+  const generateContent = async () => {};
+
   return (
-    <form onSubmit={onSubmitHandler} action="" className='flex-1 bg-blue-50/50  text-gray-600 h-full overflow-scroll'>
+    <form
+      onSubmit={onSubmitHandler}
+      action=""
+      className="flex-1 bg-blue-50/50  text-gray-600 h-full overflow-scroll"
+    >
       <div className="bg-white w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded ">
         <p className="">Upload Thumbnail</p>
         <label htmlFor="image">
-          <img src={!image ? assets.upload_area : URL.createObjectURL(image)} alt="" className='mt-2 h-16 rounded cursor-pointer'/>
-          <input onChange={(e)=>setImage(e.target.files[0])} type="file" className='' id='image' hidden required />
-
+          <img
+            src={!image ? assets.upload_area : URL.createObjectURL(image)}
+            alt=""
+            className="mt-2 h-16 rounded cursor-pointer"
+          />
+          <input
+            onChange={(e) => setImage(e.target.files[0])}
+            type="file"
+            className=""
+            id="image"
+            hidden
+            required
+          />
         </label>
 
-
-        <p className="mt-4">
-          
-          Blog Title
-        </p>
-        <input type="text" name="" id="" placeholder='Type here...' onChange={(e)=>setTitle(e.target.value)} 
-          value={title} required className='
+        <p className="mt-4">Blog Title</p>
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Type here..."
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          required
+          className="
             w-full max-w-lg mt-2 p-2  border border-gray-300 outline-none  rounded
-          '/>
+          "
+        />
 
-          <p className="mt-4">
-          
-          Sub Title
-        </p>
-        <input type="text" name="" id="" placeholder='Type here...' onChange={(e)=>setSubtitle(e.target.value)} 
-          value={subtitle} required className='
+        <p className="mt-4">Sub Title</p>
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Type here..."
+          onChange={(e) => setSubtitle(e.target.value)}
+          value={subtitle}
+          required
+          className="
             w-full max-w-lg mt-2 p-2  border border-gray-300 outline-none  rounded
-          '/>
+          "
+        />
+        <p className="mt-4">Blog Description</p>
+        <div className="max-w-lg h-74 pb-16 sm:pb-10  pt-2 relative">
+          <button type="button" onClick={generateContent} className="">
+            Generate With AI
+          </button>
+        </div>
       </div>
-
     </form>
-            
-  )
+  );
 }
