de257927eee34baae80e7d73e71dc1ae7be562f8
Rudra Verma <rudraverma781@gmail.com>
2025-09-24 08:13:56 +0530

    changes made

--- Backend/server.js
+++ Backend/server.js
@@ -8,3 +8,17 @@ const app  = express()
 
 // MiddleWare
 app.use(cors())
+app.use(express.json())
+
+
+app.get('/',(req,res)=>{
+    res.send("API WORKING !!")
+
+})
+
+const PORT = process.env.PORT ||  3000
+app.listen(PORT,()=>{
+    console.log("Server -> PORT ->",PORT)
+})
+
+export default app
