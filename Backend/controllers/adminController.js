1186435bef713635c9bbc32231bad3a3efc294f2
Rudra Verma <rudraverma781@gmail.com>
2025-09-25 11:28:27 +0530

    changes made

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -1,11 +1,18 @@
+import { response } from "express"
+import jwt from "jsonwebtoken"
+
+
 export const adminLogin=async(req,res)=>{
     try {
         const {email,pass }= req.body
          if(email != process.env.ADMIN_EMAIL || pass != process.env.ADMIN_PASS){
-            
+            return res.json({success : false , message : "Invalid Credentials"})
          }
+         const token = jwt.sign({email},process.env.JWT_SECRET)
+         res.json({success : true,token})
 
     } catch (error) {
+        res.json({success:false , message : error.message})
         
     }
 
