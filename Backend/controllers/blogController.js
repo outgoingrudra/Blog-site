eb4a2a81237333a0efff5d032aeffc70ea15e8de
Rudra Verma <rudraverma781@gmail.com>
2025-09-25 18:58:40 +0530

    changes made

--- Backend/controllers/blogController.js
+++ Backend/controllers/blogController.js
@@ -4,6 +4,9 @@ export const addBlog =async(req,res)=>{
         const {title,subTitle , description , category, isPublished , } = JSON.parse(req.body.blog)
 
         const imageFile =  req.file;
+        if(!title || !description || !category || !imageFile){
+            return res.json({success:false ,message : "Missing required fields"})
+        }
     } catch (error) {
         
     }
