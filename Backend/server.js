b35a11261aa351ee1c1c5e7b1a2f3ac67abf2c99
Rudra Verma <rudraverma781@gmail.com>
2025-09-25 18:40:30 +0530

    changes made

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import 'dotenv/config'
 import cors from "cors"
 import connectDB from "./config/db.js"
+import adminRouter from "./routes/adminRoutes.js"
 
 
 const app  = express()
@@ -12,11 +13,12 @@ await connectDB()
 app.use(cors())
 app.use(express.json())
 
-
+// Routes 
 app.get('/',(req,res)=>{
     res.send("API WORKING !!")
 
 })
+app.use('/api/admin',adminRouter)
 
 const PORT = process.env.PORT ||  3000
 app.listen(PORT,()=>{
