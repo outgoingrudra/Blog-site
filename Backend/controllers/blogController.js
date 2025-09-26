1cfaad62d2ea1be4f2e5d0db191f053abf206647
Rudra Verma <rudraverma781@gmail.com>
2025-09-26 08:12:29 +0530

    changes made

--- Backend/controllers/blogController.js
+++ Backend/controllers/blogController.js
@@ -1,6 +1,9 @@
 import fs from "fs"
 import imageKit from "../config/ImageKit.js";
 import { format } from "path";
+import Blog from "../models/Blog.js"
+
+
 export const addBlog =async(req,res)=>{
     try {
         const {title,subTitle , description , category, isPublished , } = JSON.parse(req.body.blog)
@@ -23,12 +26,24 @@ export const addBlog =async(req,res)=>{
         const optimizedImageURL = imageKit.url({
             path: response.filePath,
             transformation :[
-                {quality : 'auto'},
+                {quality : 'auto'},// auto comprrssion
                 {format : 'webp'},
                 { width:'1280'}
             ]
         })
+
+        const image = optimizedImageURL
+
+        await Blog.create({
+            title,subTitle,description,category,image,isPublished
+        })
+
+        res.json({
+            success:true, message : "Blog Added Successfully"
+        })
     } catch (error) {
-        
+        res.json({
+            success:false, message : error.message
+        })
     }
 }
