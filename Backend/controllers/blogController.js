23d9fbeee1d4636a8d57934d21d659f2d5af73d6
Rudra Verma <rudraverma781@gmail.com>
2025-09-26 08:04:40 +0530

    changes made

--- Backend/controllers/blogController.js
+++ Backend/controllers/blogController.js
@@ -1,4 +1,5 @@
-
+import fs from "fs"
+import imageKit from "../config/ImageKit.js";
 export const addBlog =async(req,res)=>{
     try {
         const {title,subTitle , description , category, isPublished , } = JSON.parse(req.body.blog)
@@ -7,6 +8,15 @@ export const addBlog =async(req,res)=>{
         if(!title || !description || !category || !imageFile){
             return res.json({success:false ,message : "Missing required fields"})
         }
+
+        const fileBuffer = fs.readFileSync(imageFile.path)
+
+        //upload image to imagekit
+        const response = await imageKit.upload({
+            file : fileBuffer,
+            fileName : imageFile.originalName,
+            folder: "/blogs"
+        })
     } catch (error) {
         
     }
