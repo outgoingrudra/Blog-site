9d6f6d4c6b746313182e159efab1daba10602e1b
Rudra Verma <rudraverma781@gmail.com>
2025-09-28 15:36:08 +0530

    changes made

--- Backend/models/Comment.js
+++ Backend/models/Comment.js
@@ -1,7 +1,10 @@
 import mongoose  from "mongoose";
 
 const Commentchema = new mongoose.Schema({
-  blog : {type: mongoose.Schema.Types.ObjectId,ref:'blog',required : true}
+  blog : {type: mongoose.Schema.Types.ObjectId,ref:'blog',required : true},
+  name : {type:String , required : true},
+  content : {type:String , required : true },
+  isApproved : {type:Boolean , required : false }
 },{timestamps:true})
 
 const Comment = mongoose.model('Comment',Commentchema)
