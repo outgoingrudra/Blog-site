c0b37730786dafb1598a671639084bb37881828c
Rudra Verma <rudraverma781@gmail.com>
2025-09-29 20:51:49 +0530

    changes made

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import  {AppProvider } from "./context/AppContext.js"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { AppProvider } from "./context/AppContext.js";
 
-createRoot(document.getElementById('root')).render(
-<BrowserRouter>
-<App/>
-</BrowserRouter>
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </BrowserRouter>
+);
