92ce1924507172555c977004fe1fad9cee414e67
Rudra Verma <rudraverma781@gmail.com>
2025-09-29 20:32:05 +0530

    changes made

--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -1,8 +1,15 @@
 import express from 'express'
-import { adminLogin } from '../controllers/adminController.js'
+import auth from "../middlewares/auth.js"
+import {adminLogin, approveCommentByID, deleteCommentById, getAllBlogsAdmin, getAllComments, getDashBoard} from "../controllers/adminController.js"
 
 const adminRouter = express.Router()
 
 adminRouter.post('/login',adminLogin)
+adminRouter.get("/comments",auth , getAllComments)
+adminRouter.get("/blogs",auth , getAllBlogsAdmin)
+adminRouter.post("/delete-comment",auth , deleteCommentById)
+adminRouter.post("/approve-comment",auth , approveCommentByID)
+adminRouter.get("/dashboard",auth , getDashBoard)
+
 
 export default adminRouter
