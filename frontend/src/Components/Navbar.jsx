5db9ca66b3d1a2c180e555b5471a36000c0e4c72
Rudra Verma <rudraverma781@gmail.com>
2025-09-30 14:40:00 +0530

    changes made

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import assets from "../assets/assets"
-import { useNavigate } from "react-router-dom"   // ✅ fix import
+
+import {useAppContext} from "../context/AppContext.jsx"
 
 export default function Navbar() {
-  const navigate = useNavigate()
+  const {navigate , token  } = useAppContext()
 
   return (
     <div className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32 cursor-pointer '>
@@ -18,7 +19,9 @@ export default function Navbar() {
         onClick={() => navigate('/admin')}   // ✅ fixed bracket
         className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5'
       >
-        Login
+        {
+          token ? 'Dashboard'  : 'Login'
+        }
         <img src={assets.arrow} alt="arrow" className='w-3' />
       </button>
     </div>
